48a9b817f3e9d4c26e1ccd417d407cab437038c2
dmzx <info@dmzx-web.net>
2015-12-13 00:25:57 +0100

    Update version 1.0.1

--- migrations/update_table.php
+++ migrations/update_table.php
@@ -12,9 +12,10 @@ class update_table extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		return array(
-		 array('custom', array(array($this, 'install_bbcode_for_travisbbcode'))),
+			array('custom', array(array($this, 'install_bbcode_for_travisbbcode'))),
 		);
 	}
+
 	public function revert_data()
 	{
 		return array(
@@ -27,32 +28,32 @@ class update_table extends \phpbb\db\migration\migration
 
 		if (!class_exists('acp_bbcodes'))
 		{
-		 include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
+			include($this->phpbb_root_path . 'includes/acp/acp_bbcodes.' . $this->php_ext);
 		}
 		$bbcode_tool = new \acp_bbcodes();
 
 		$bbcode_name = 'travis';
 		$bbcode_array = array(
-		 'bbcode_match'		=> '[travis]{IDENTIFIER1}/{IDENTIFIER2}[/travis]',
-		 'bbcode_tpl'		=> '<div><a href="https://travis-ci.org/{IDENTIFIER1}/{IDENTIFIER2}" title="{IDENTIFIER1}/{IDENTIFIER2}"><img src="https://travis-ci.org/{IDENTIFIER1}/{IDENTIFIER2}.svg?branch=master" alt="" style="margin-bottom: -5px;" /></a></div>',
-		 'bbcode_helpline'	=> '[travis]yourvendor/yourextension[/travis]',
-		 'display_on_posting'	=> 1
+			'bbcode_match'		=> '[travis]{IDENTIFIER1}/{IDENTIFIER2}[/travis]',
+			'bbcode_tpl'		=> '<a href="https://travis-ci.org/{IDENTIFIER1}/{IDENTIFIER2}" title="{IDENTIFIER1}/{IDENTIFIER2}"><img src="https://travis-ci.org/{IDENTIFIER1}/{IDENTIFIER2}.svg?branch=master" alt="" style="margin-bottom: -5px;" /></a>',
+			'bbcode_helpline'	=> '[travis]yourvendor/yourextension[/travis]',
+			'display_on_posting'	=> 1
 		);
 
 		$data = $bbcode_tool->build_regexp($bbcode_array['bbcode_match'], $bbcode_array['bbcode_tpl'], $bbcode_array['bbcode_helpline']);
 
 		$bbcode_array += array(
-		 'bbcode_tag'			=> $data['bbcode_tag'],
-		 'first_pass_match'	=> $data['first_pass_match'],
-		 'first_pass_replace'	=> $data['first_pass_replace'],
-		 'second_pass_match'	=> $data['second_pass_match'],
-		 'second_pass_replace' => $data['second_pass_replace']
+			'bbcode_tag'			=> $data['bbcode_tag'],
+			'first_pass_match'		=> $data['first_pass_match'],
+			'first_pass_replace'	=> $data['first_pass_replace'],
+			'second_pass_match'		=> $data['second_pass_match'],
+			'second_pass_replace' 	=> $data['second_pass_replace']
 		);
 
 		$sql = 'SELECT bbcode_id
-		 FROM ' . $this->table_prefix . "bbcodes
-		 WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
-		 OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
+			FROM ' . $this->table_prefix . "bbcodes
+			WHERE LOWER(bbcode_tag) = '" . strtolower($bbcode_name) . "'
+			OR LOWER(bbcode_tag) = '" . strtolower($bbcode_array['bbcode_tag']) . "'";
 		$result = $this->db->sql_query($sql);
 		$row_exists = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
@@ -60,48 +61,42 @@ class update_table extends \phpbb\db\migration\migration
 		if ($row_exists)
 		{
 
-		 $bbcode_id = $row_exists['bbcode_id'];
+			$bbcode_id = $row_exists['bbcode_id'];
 
-		 $sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
-			SET ' . $this->db->sql_build_array('UPDATE', $bbcode_array) . '
-			WHERE bbcode_id = ' . $bbcode_id;
-		 $this->db->sql_query($sql);
+			$sql = 'UPDATE ' . $this->table_prefix . 'bbcodes
+				SET ' . $this->db->sql_build_array('UPDATE', $bbcode_array) . '
+				WHERE bbcode_id = ' . $bbcode_id;
+			$this->db->sql_query($sql);
 		}
 		else
 		{
 
-		 $sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
-			FROM ' . $this->table_prefix . 'bbcodes';
-		 $result = $this->db->sql_query($sql);
-		 $row = $this->db->sql_fetchrow($result);
-		 $this->db->sql_freeresult($result);
+			$sql = 'SELECT MAX(bbcode_id) AS max_bbcode_id
+				FROM ' . $this->table_prefix . 'bbcodes';
+			$result = $this->db->sql_query($sql);
+			$row = $this->db->sql_fetchrow($result);
+			$this->db->sql_freeresult($result);
 
-		 if ($row)
-		 {
-			$bbcode_id = $row['max_bbcode_id'] + 1;
+			if ($row)
+			{
+				$bbcode_id = $row['max_bbcode_id'] + 1;
 
-			if ($bbcode_id <= NUM_CORE_BBCODES)
+				if ($bbcode_id <= NUM_CORE_BBCODES)
+				{
+					$bbcode_id = NUM_CORE_BBCODES + 1;
+				}
+			}
+			else
 			{
 				$bbcode_id = NUM_CORE_BBCODES + 1;
 			}
-		 }
-		 else
-		 {
-			$bbcode_id = NUM_CORE_BBCODES + 1;
-		 }
 
-		 if ($bbcode_id <= BBCODE_LIMIT)
-		 {
-			$bbcode_array['bbcode_id'] = (int) $bbcode_id;
+			if ($bbcode_id <= BBCODE_LIMIT)
+			{
+				$bbcode_array['bbcode_id'] = (int) $bbcode_id;
 
-			$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array('INSERT', $bbcode_array));
-		 }
+				$this->db->sql_query('INSERT INTO ' . $this->table_prefix . 'bbcodes ' . $this->db->sql_build_array('INSERT', $bbcode_array));
+			}
 		}
-	}
-
-	public function remove_bbcode()
-	{
-		$sql = 'DELETE FROM ' . BBCODES_TABLE . ' WHERE bbcode_tag = \'travis\'';
-		$this->db->sql_query($sql);
 	}
 }
